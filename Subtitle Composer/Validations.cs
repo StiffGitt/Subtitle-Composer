6ec2bfae67cca3e79d5a9b04db7d38bdee1df3f0
StiffGitt <131384247+StiffGitt@users.noreply.github.com>
2023-06-06 21:17:04 +0200

    Add project files.

---
+++ Subtitle Composer/Validations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Subtitle_Composer
+{
+    public class TimeSpanValidation : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            //string s = (string)value;
+            if ((string) value == "" || !TimeSpanFormatting.StringToTimeSpan((string)value).Item1)
+                return new ValidationResult(false, "not a timespan");
+            else
+                return ValidationResult.ValidResult;
+        }
+    }
+}
